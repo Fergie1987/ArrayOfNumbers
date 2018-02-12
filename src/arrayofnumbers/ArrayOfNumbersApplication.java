0284765918cc289e51c918b000eddc119e13b9cf
fergi <fergi@DESKTOP-9EMP2HU>
2018-02-12 13:48:41 +0000

    First Push

---
+++ src/arrayofnumbers/ArrayOfNumbersApplication.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayofnumbers;
+
+/**
+ *
+ * @author fergi
+ */
+public class ArrayOfNumbersApplication {
+    
+    public static void main(String[] args) {
+        //ArrayOfNumbers a = new ArrayOfNumbers();
+       // a.setItem();
+        
+        int [] newArray={5,25,3,4,5};
+        
+        ArrayOfNumbers b = new ArrayOfNumbers(newArray);
+        b.count();
+        b.average();
+    }
+}
